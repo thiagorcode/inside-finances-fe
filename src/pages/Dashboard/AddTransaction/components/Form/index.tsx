b5013f9db9d1c9a22b75669f8e0f556924ac3ec1
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-29 18:40:28 -0300

    feat: add formik and yup

--- src/pages/Dashboard/AddTransaction/components/Form/index.tsx
+++ src/pages/Dashboard/AddTransaction/components/Form/index.tsx
@@ -1,17 +1,44 @@
+import { format } from 'date-fns';
+import { useFormik } from 'formik';
 import { Input } from '../../../../../components/Form';
 import { FormTransaction } from '../../styles';
-import { format } from 'date-fns';
 import { Categorys } from '../Categorys';
 import Button from '@/components/Button';
 interface FormProps {
   setStep: (value: 0 | 1) => void;
 }
+
+const initialValues = {
+  type: '',
+  description: '',
+  category: '',
+  value: null,
+  date: null,
+};
+
 export const Form = ({ setStep }: FormProps) => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement> | undefined) => {
+    console.log(onSubmit);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    handleSubmit: onSubmit,
+  });
+
   return (
     <FormTransaction>
       {/** TODO: Alterar o placeholder quando ele for despesa ou receita */}
-      <Input label="Descrição:" placeholder="Alimentação, Salário, Conta" />
-      <Input label="Tipo:" placeholder="Alimentação, Salário, Conta">
+      <Input
+        label="Descrição:"
+        placeholder="Alimentação, Salário, Conta"
+        name="description"
+      />
+      <Input
+        label="Tipo:"
+        placeholder="Alimentação, Salário, Conta"
+        name="type"
+      >
         <select className="class_input">
           <option value="">Selecione</option>
           <option value="+">Receita</option>
