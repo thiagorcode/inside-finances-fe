47d6c9f301fb0b4e5be2f52ce7d2e4e20a942fa6
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2022-12-29 19:08:03 -0300

    up

--- src/pages/Auth/Transactions/transactions.general.tsx
+++ src/pages/Auth/Transactions/transactions.general.tsx
@@ -3,6 +3,10 @@ import { Grid } from '@mui/material';
 import { Table } from 'antd';
 import MobileMenu from '../../../components/MobileMenu';
 import { columns } from './columns';
+import { useCallback, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useModal } from '@/context/modal';
+import { getTransactionsByParams } from '@/services/transactions/transactions.service';
 
 const data = [
   {
@@ -15,6 +19,41 @@ const data = [
 ];
 
 export const TransactionsGeneral = () => {
+
+  const { toggleModal } = useModal();
+
+  const [transactions, setTransactions] = useState<any[]>([]);
+  const [hasNext, setHasNext] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const handleOpenModal = () => {
+    toggleModal({
+      addTransaction: {
+        isOpen: true,
+      },
+    });
+  };
+
+  const loadTransactions = useCallback(async () => {
+    try {
+      const { data } = await getTransactionsByParams({
+        limit: 50,
+        page,
+        userId: 'fff',
+      });
+      console.log(data);
+
+      setTransactions(value => [...value, ...data.transactions.transactions]);
+      setHasNext(data.transactions.hasNext);
+      setPage(value => value + 1);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
   return (
     <>
       <Header />
