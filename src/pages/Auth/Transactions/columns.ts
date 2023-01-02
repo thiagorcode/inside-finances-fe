36ea1ef49b3b0aad287d34edfbdf11976f5ac265
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2023-01-02 18:30:35 -0300

    feat: format date

--- src/pages/Auth/Transactions/columns.ts
+++ src/pages/Auth/Transactions/columns.ts
@@ -1,6 +1,7 @@
 import { Transactions } from '@/interface/transactions.interface';
 import { formatMoney } from '@/utils/formatMoney';
 import { ColumnsType } from 'antd/es/table';
+import { format, parse } from 'date-fns';
 
 export const columns: ColumnsType<Transactions> = [
   // {
@@ -20,6 +21,9 @@ export const columns: ColumnsType<Transactions> = [
     title: 'Data',
     dataIndex: 'date',
     key: 'date',
+    render(value) {
+      return format(parse(value, 'yyyy-MM-dd', new Date()), 'dd/MM/yyyy');
+    },
   },
 
   {
