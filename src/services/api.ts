3df1421a832dbe3f314595a4a79cc6333cea3424
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2023-01-18 16:48:07 -0300

    chore: changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 console.log(import.meta.env.API_URL);
 export default axios.create({
-  baseURL: import.meta.env.API_URL || 'http://54.94.37.103/api',
+  baseURL: import.meta.env.API_URL || 'https://test.apifinances.xyz/api',
   headers: {
     'Content-type': 'application/json',
   },
