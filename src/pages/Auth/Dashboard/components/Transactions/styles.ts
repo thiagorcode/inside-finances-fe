f70db1c3451fb491ec4e8f126b1f272f2a5aaafa
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2023-01-20 13:48:26 -0300

    style

--- src/pages/Auth/Dashboard/components/Transactions/styles.ts
+++ src/pages/Auth/Dashboard/components/Transactions/styles.ts
@@ -12,8 +12,8 @@ export const Resume = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    padding-bottom: 0.5rem;
-    padding-top: 18px;
+
+    padding: 1.1rem 0;
     border-bottom: 0.1px solid #b5b5b5;
     color: #858aa1;
     width: 100%;
