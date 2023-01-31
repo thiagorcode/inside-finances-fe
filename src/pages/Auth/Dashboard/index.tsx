a9306c4c92dd76eb9cdafc75f7c31555ea4a5c0f
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2023-01-31 18:29:01 -0300

    too

--- src/pages/Auth/Dashboard/index.tsx
+++ src/pages/Auth/Dashboard/index.tsx
@@ -1,9 +1,5 @@
 import { Box } from '@mui/system';
 
-import Header from '../../../components/Header';
-import MobileMenu from '../../../components/MobileMenu';
-import Button from '../../../components/Button';
-
 import { AddTransaction } from './AddTransaction';
 import { Transactions } from './components/Transactions';
 import { Investments } from './components/Investments';
@@ -14,7 +10,6 @@ const Dashboard = () => {
   const { modal } = useModal();
   return (
     <>
-      <Header />
       <Box
         component="div"
         sx={{
@@ -26,7 +21,6 @@ const Dashboard = () => {
         <Investments />
         {/* <Cards />  */}
       </Box>
-      <MobileMenu />
       {modal?.addTransaction?.isOpen && <AddTransaction />}
     </>
   );
