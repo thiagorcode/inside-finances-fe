9eb624304f9fe3c9b70fe00d7a31cc4362c0f7cf
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2024-02-27 14:18:46 -0300

    refactor: lint and added interface

--- src/components/ModalMonthly/index.tsx
+++ src/components/ModalMonthly/index.tsx
@@ -2,60 +2,65 @@ import React, { useState } from 'react';
 import * as S from './style';
 import { IoClose } from 'react-icons/io5';
 
-interface Imodal {
+interface IModal {
   openModal: boolean;
   setOpenModal: (openModal: boolean) => void;
-  datasMonthly:any;
+  dataMonthly: {
+    expenseValue: number;
+    goal: number;
+    id: string;
+    userId: string;
+    year: string;
+    yearMonth: string;
+  };
 }
-
-
 
 export default function ModalMonthly({
   openModal,
   setOpenModal,
-  datasMonthly,
-}: Imodal) {
-  
-  const [modificationMonthly, setModificationMonthly] = useState('')
+  dataMonthly,
+}: IModal) {
+  const [modificationMonthly, setModificationMonthly] = useState('');
   const options = {
     minimumFractionDigits: 2,
   };
 
-  function Handlevaluegoal(e: { target: { value: React.SetStateAction<string>; }; }){
-    setModificationMonthly(e.target.value)
+  function handleValueGoal(e: {
+    target: { value: React.SetStateAction<string> };
+  }) {
+    setModificationMonthly(e.target.value);
   }
 
-  
-  function HandleSaveClick() {
-    if(modificationMonthly == ''){
-      alert ('insira alguma meta')
-    }else{
+  function handleSaveClick() {
+    if (modificationMonthly == '') {
+      alert('insira alguma meta');
+    } else {
       setOpenModal(!openModal);
     }
   }
 
   if (openModal) {
     return (
-
       <S.Background>
         <S.ContainerModal>
-              <>
-                <S.ButtonClose >
-                  <button onClick={() => setOpenModal(!openModal)}>
-                    <IoClose style={{ color: 'white' }} size={40} />
-                  </button>
-                </S.ButtonClose>
-                <S.InputsPart>
-                  <span>Meta mensal</span>
-                  <input type="text" 
-                    value={modificationMonthly}
-                    onChange={Handlevaluegoal}
-                    placeholder={datasMonthly?.goal.toLocaleString('pt-br', options)}
-                  ></input>
-                  <button onClick={HandleSaveClick}>Salvar</button>
-                </S.InputsPart>
-              </>
-            </S.ContainerModal>
+          <>
+            <S.ButtonClose>
+              <button onClick={() => setOpenModal(!openModal)}>
+                <IoClose style={{ color: 'white' }} size={40} />
+              </button>
+            </S.ButtonClose>
+            <S.InputsPart>
+              <span>Meta mensal</span>
+              <input
+                type="text"
+                value={modificationMonthly}
+                onChange={handleValueGoal}
+                placeholder={dataMonthly?.goal.toLocaleString('pt-br', options)}
+              ></input>
+              <button onClick={handleSaveClick}>Salvar</button>
+            </S.InputsPart>
+          </>
+        </S.ContainerModal>
       </S.Background>
     );
   } else {
--- src/pages/Auth/Home/components/MonthlyGoals/index.tsx
+++ src/pages/Auth/Home/components/MonthlyGoals/index.tsx
@@ -6,54 +6,54 @@ import Goal from '../../../../../../assets/svg/octicongoal-16.svg';
 import ReactSpeedometer from 'react-d3-speedometer';
 import ModalMonthly from '@/components/ModalMonthly';
 import { useUser } from '@/hooks/useUser';
-import api from '../../../../../services/api'
+import api from '../../../../../services/api';
 import { display } from '@mui/system';
 
 export interface IDataMonthly {
-
-  expenseValue: number
-  goal: number
-  id: string
-  recipeValue: number
-  total: number
-  userId: string
-  year: string
-  yearMonth: string
+  expenseValue: number;
+  goal: number;
+  id: string;
+  recipeValue: number;
+  total: number;
+  userId: string;
+  year: string;
+  yearMonth: string;
 }
 
-
-export default  function MonthlyGoals() {
-  const [openModal, setOpenModal] = useState<boolean>(false)
+export default function MonthlyGoals() {
+  const [openModal, setOpenModal] = useState<boolean>(false);
   const { userAccess } = useUser();
-  const [datasMonthly, setDatasMonthly] = useState <IDataMonthly | null>(null)
+  const [dataMonthly, setDataMonthly] = useState<IDataMonthly | null>(null);
 
-  const DateAt = new Date();
-  const CapturMonth = DateAt.getMonth() + 1;
-  const MonthFormatted = CapturMonth <= 9 ? '0' + CapturMonth : CapturMonth;
-  const Year = new Date().toLocaleDateString('pt-br', { year: 'numeric' });
+  const dateAt = new Date();
+  const captureMonth = dateAt.getMonth() + 1;
+  const monthFormatted = captureMonth <= 9 ? '0' + captureMonth : captureMonth;
+  const year = new Date().toLocaleDateString('pt-br', { year: 'numeric' });
   const options = {
     minimumFractionDigits: 2,
   };
-  const Month = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
+  const month = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
   ];
-  const selectMonth = Month[DateAt.getMonth()]
-  
+  const selectMonth = month[dateAt.getMonth()];
+
   async function fetchData() {
     try {
-      const response = await api.get(`/transactions/reports-monthly?userId=${userAccess.id}&yearMonth=${Year}-${MonthFormatted}`);
-      setDatasMonthly(response.data.reportMonthly);
+      const response = await api.get(
+        `/transactions/reports-monthly?userId=${userAccess.id}&yearMonth=${year}-${monthFormatted}`,
+      );
+      setDataMonthly(response.data.reportMonthly);
     } catch (error) {
       console.error('Error fetching monthly reports:', error);
     }
@@ -62,82 +62,86 @@ export default  function MonthlyGoals() {
   useEffect(() => {
     fetchData();
   }, []);
-  
 
   return (
-  
     <div>
-  {!datasMonthly ? (
-    <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
-      <h1>carregando ..</h1>
-    </div>
-  ) : (
-    <S.ContainerCard>
-      <>
-        <S.MonthlyValue>
-          <div>
-            <S.Valuespan>
-              R${' '}
-              {datasMonthly?.recipeValue.toLocaleString('pt-br', options)}
-            </S.Valuespan>
-            <button onClick={() => setOpenModal(!openModal)}>
-              <img src={IconMonthly} alt="Caneta para modificar" />
-            </button>
-            <ModalMonthly
-              datasMonthly={datasMonthly}
-              openModal={openModal}
-              setOpenModal={setOpenModal}
-            />
-          </div>
-          <S.Datespan>
-            {selectMonth}, {datasMonthly?.year}
-          </S.Datespan>
-        </S.MonthlyValue>
-        <S.BarDivision />
-        <div style={{ display: 'flex' }}>
-          <S.Group>
-            <div>
-              <S.ImgStyle>
-                <img src={Award} alt="Medalha" />
-                <p> Valor atual</p>
-              </S.ImgStyle>
-              <span>
-                R$
-                {datasMonthly?.recipeValue.toLocaleString('pt-br', options)}
-              </span>
-            </div>
-            <div style={{ marginTop: '20px' }}>
-              <S.ImgStyle>
-                <img src={Goal} alt="Aro de alvo" />
-                <p> Meta mensal</p>
-              </S.ImgStyle>
-              <span>
-                R$
-                {datasMonthly?.goal.toLocaleString('pt-br', {
-                  minimumFractionDigits: 2,
-                })}
-              </span>
-            </div>
-          </S.Group>
-          <S.DirectionSpeedMater>
-            <ReactSpeedometer
-              width={180}
-              height={115}
-              maxValue={datasMonthly?.goal}
-              maxSegmentLabels={1}
-              segments={6}
-              value={datasMonthly?.recipeValue}
-              startColor={'#293DC7'}
-              endColor={'#2f52d4 '}
-              needleColor="#001dd8"
-            />
-            <S.Stylespan style={{ fontSize: '12px' }}>
-              valor atual vs meta mensal
-            </S.Stylespan>
-          </S.DirectionSpeedMater>
+      {!dataMonthly ? (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'center',
+          }}
+        >
+          <h1>carregando ..</h1>
         </div>
-      </>
-    </S.ContainerCard>
-  )}
-</div>
-  )}
+      ) : (
+        <S.ContainerCard>
+          <>
+            <S.MonthlyValue>
+              <div>
+                <S.Valuespan>
+                  R$ {dataMonthly?.recipeValue.toLocaleString('pt-br', options)}
+                </S.Valuespan>
+                <button onClick={() => setOpenModal(!openModal)}>
+                  <img src={IconMonthly} alt="Caneta para modificar" />
+                </button>
+                <ModalMonthly
+                  dataMonthly={dataMonthly}
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                />
+              </div>
+              <S.Datespan>
+                {selectMonth}, {dataMonthly?.year}
+              </S.Datespan>
+            </S.MonthlyValue>
+            <S.BarDivision />
+            <div style={{ display: 'flex' }}>
+              <S.Group>
+                <div>
+                  <S.ImgStyle>
+                    <img src={Award} alt="Medalha" />
+                    <p> Valor atual</p>
+                  </S.ImgStyle>
+                  <span>
+                    R$
+                    {dataMonthly?.recipeValue.toLocaleString('pt-br', options)}
+                  </span>
+                </div>
+                <div style={{ marginTop: '20px' }}>
+                  <S.ImgStyle>
+                    <img src={Goal} alt="Aro de alvo" />
+                    <p> Meta mensal</p>
+                  </S.ImgStyle>
+                  <span>
+                    R$
+                    {dataMonthly?.goal.toLocaleString('pt-br', {
+                      minimumFractionDigits: 2,
+                    })}
+                  </span>
+                </div>
+              </S.Group>
+              <S.DirectionSpeedMater>
+                <ReactSpeedometer
+                  width={180}
+                  height={115}
+                  maxValue={dataMonthly?.goal}
+                  maxSegmentLabels={1}
+                  segments={6}
+                  value={dataMonthly?.recipeValue}
+                  startColor={'#293DC7'}
+                  endColor={'#2f52d4 '}
+                  needleColor="#001dd8"
+                />
+                <S.Stylespan style={{ fontSize: '12px' }}>
+                  valor atual vs meta mensal
+                </S.Stylespan>
+              </S.DirectionSpeedMater>
+            </div>
+          </>
+        </S.ContainerCard>
+      )}
+    </div>
+  );
+}
