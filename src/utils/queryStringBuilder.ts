e4a65638282dd9429ddff1fff9f3b9bc4ff37177
Thiago Rodrigues <ti.thiago.rodrigues@outlook.com>
2024-03-18 21:34:03 -0300

    up

--- src/utils/queryStringBuilder.ts
+++ src/utils/queryStringBuilder.ts
@@ -1,7 +1,7 @@
 export const queryStringBuilder = (obj: any) => {
   const str = [];
   for (const p in obj)
-    if (obj[p] != undefined) {
+    if (obj[p] !== undefined) {
       str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
     }
   return str.join('&');
